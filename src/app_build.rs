0b271d321e646af515246e1d4b5885bb2e8f6bb6
Rob Dobson <rob@dobson.com>
2024-03-14 22:16:22 -0400

    Fixed clean only option

--- src/app_build.rs
+++ src/app_build.rs
@@ -12,6 +12,8 @@ pub fn build_raft_app(build_sys_type: &Option<String>, clean: bool, clean_only: 
             no_docker_arg: bool, idf_path_full: Option<String>) 
                             -> Result<String, Box<dyn std::error::Error>> {
 
+    // println!("Building the app in folder: {} clean {} clean_only {} no_docker_arg {}", app_folder, clean, clean_only, no_docker_arg);
+
     // Check the app folder is valid
     if !check_app_folder_valid(&app_folder) {
         return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Invalid app folder")));
@@ -40,11 +42,6 @@ pub fn build_raft_app(build_sys_type: &Option<String>, clean: bool, clean_only: 
         }
     }
 
-    // Check if clean_only is true
-    if clean_only {
-        return Ok("Clean completed".to_string());
-    }
-
     // Build the app
     println!("Building the SysType {} app in folder: {} delete_build {} delete_raft_artifacts {}", 
                         sys_type, app_folder, delete_build_folder, delete_build_raft_artifacts_folder);
@@ -61,12 +58,12 @@ pub fn build_raft_app(build_sys_type: &Option<String>, clean: bool, clean_only: 
         let idf_path = idf_path_full.unwrap_or("idf.py".to_string());
 
         // Build without docker
-        build_without_docker(&app_folder, &sys_type, clean, 
+        build_without_docker(&app_folder, &sys_type, clean, clean_only,
                     delete_build_folder, delete_build_raft_artifacts_folder, idf_path)
     } else {
         // Build with docker
-        build_with_docker(&app_folder, &sys_type, 
-                    clean, delete_build_folder, delete_build_raft_artifacts_folder)
+        build_with_docker(&app_folder, &sys_type, clean, clean_only,
+                    delete_build_folder, delete_build_raft_artifacts_folder)
     };
 
     // Check for build error
@@ -84,7 +81,7 @@ pub fn build_raft_app(build_sys_type: &Option<String>, clean: bool, clean_only: 
 }
 
 // Build with docker and return output as a string
-fn build_with_docker(project_dir: &str, systype_name: &str, clean: bool,
+fn build_with_docker(project_dir: &str, systype_name: &str, clean: bool, clean_only: bool,
             delete_build_folder: bool, delete_raft_artifacts_folder: bool) -> Result<String, std::io::Error> {
     // Build with docker
     println!("Building with docker in {} for SysType {} clean {}", project_dir, systype_name, clean);
@@ -126,7 +123,9 @@ fn build_with_docker(project_dir: &str, systype_name: &str, clean: bool,
     if clean {
         command_sequence += " fullclean";
     }
-    command_sequence += " build";
+    if !clean_only {
+        command_sequence += " build";
+    }
 
     let docker_run_args = vec![
         "run", "--rm",
@@ -156,7 +155,7 @@ fn build_with_docker(project_dir: &str, systype_name: &str, clean: bool,
 }
 
 // Build without docker
-fn build_without_docker(project_dir: &str, systype_name: &str, clean: bool,
+fn build_without_docker(project_dir: &str, systype_name: &str, clean: bool, clean_only: bool,
     delete_build_folder: bool, delete_raft_artifacts_folder: bool,
     idf_path: String) -> Result<String, std::io::Error> {
     
@@ -187,7 +186,9 @@ fn build_without_docker(project_dir: &str, systype_name: &str, clean: bool,
     if clean {
         idf_run_args.push("fullclean".to_string());
     }
-    idf_run_args.push("build".to_string());
+    if !clean_only {
+        idf_run_args.push("build".to_string());
+    }
 
     // Command
     let idf_command = idf_path.as_str();
